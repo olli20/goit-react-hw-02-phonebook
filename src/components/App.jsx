af6e50c358c07d19262ce0ab7bfeb1a51aabfa01
Olena Voina <105532756+olli20@users.noreply.github.com>
2023-01-21 18:17:35 +0100

    form adds new contact, alert

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-
+import { nanoid } from "nanoid";
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
@@ -7,7 +7,6 @@ import ContactList from './ContactList';
 import styles from './app.module.scss';
 
 class App extends Component {
-
   state = {
     contacts: [
       {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
@@ -15,6 +14,8 @@ class App extends Component {
       {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
       {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ],
+    name: "sdf",
+    number: "dfg",
   }
 
   deleteContact = (contactId) => {
@@ -22,13 +23,45 @@ class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }))
   }
+
+  handleInputChange = event => {
+    const {name, value} = event.currentTarget;
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    const {contacts, name, number} = this.state;
+
+    const isNameAlreadyHere = contacts.filter(contact => contact.name === name).length;
+    if(isNameAlreadyHere){
+      alert('Вже є');
+      return;
+    };
+
+    const newContact = {
+      name: name,
+      number: number,
+      id: nanoid(),
+    }
+
+    this.setState(prevState => {
+      return {contacts: [...prevState.contacts, newContact]};
+    });
+  };
   
   render() {
-    const {contacts} = this.state;
+    const {contacts, name, number} = this.state;
     return (
       <div className={styles.app}>
         <h1>Phonebook</h1>
-        <ContactForm />
+        <ContactForm 
+          name={name}
+          number={number}
+          onFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange} />
 
         <h2>Contacts</h2>
         <Filter  />
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,26 +1,30 @@
 import styles from './contact-form.module.scss';
 
-const ContactForm = () => {
+const ContactForm = ({handleInputChange, onFormSubmit, name, number}) => {
     return (
-        <form className={styles.form}>
+        <form onSubmit={onFormSubmit} className={styles.form}>
             <div className={styles.inputBlock}>
                 <label htmlFor="name" className={styles.name}>Name</label>
                 <input
                     type="text"
                     name="name"
+                    value={name}
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                     required
-                    />
+                    onChange={handleInputChange}
+                />
             </div>
             <div className={styles.inputBlock}>
                 <label htmlFor="number" className={styles.name}>Number</label>
                 <input
                     type="tel"
                     name="number"
+                    value={number}
                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                     required
+                    onChange={handleInputChange}
                 />
             </div>
             <button type="submit">Add contact</button>
